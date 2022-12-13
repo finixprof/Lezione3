cad207aeb5101c61cda3e12c83e7e7d44de5e284
Salvo Finistrella <salvo.finistrella@iispascal.it>
2022-12-13 12:46:15 +0100

    Aggiunta interface IAnimale

---
+++ GenericAndInterface/Cane.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lezione3.GenericAndInterface
+{
+    class Cane : Generico<int>, IAnimale //cane eredita Generico di tipo int e implementa interfaccia IAnimale
+    {
+        public string FaiIlVerso()
+        {
+            return "Bau bau";
+        }
+    }
+}
--- GenericAndInterface/Generico.cs
+++ GenericAndInterface/Generico.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GenericAndInterface
+namespace Lezione3.GenericAndInterface
 {
     public class Generico<T>
     {
